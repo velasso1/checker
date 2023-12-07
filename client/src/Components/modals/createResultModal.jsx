d0edcf7090fc14fe2ec0b7831d598b4c17074876
Alexandr Vasin <alexandr.vasin1@yandex.ru>
2023-12-07 17:17:43 +0300

    remove unusued vars

--- client/src/Components/modals/createResultModal.jsx
+++ client/src/Components/modals/createResultModal.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { createResult } from "../../store/slices/dataSlice";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import SuccessStatus from "./successStatus";
 import { fetchingData } from "../../store/slices/dataSlice";
 import TextField from "../fields/textField";
